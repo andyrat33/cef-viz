b6292821ebbfb0e6f76bc1079baee9482fa21043
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2017-01-09 22:29:41 +0000

    updated to use pygal and remove the google graph and moved to python3 env

--- cef-viz.py
+++ cef-viz.py
@@ -3,10 +3,10 @@ app = Flask(__name__)
 
 import os
 import json
-import urllib2
 import redis
 import datetime
 import pygal
+from pygal.style import DarkSolarizedStyle
 
 rconfig = {
     'host': 'docker2',
@@ -16,8 +16,10 @@ rconfig = {
 
 r = redis.StrictRedis(**rconfig)
 # Time Range for Selection - hardcoded for now
-zrStart = int(datetime.datetime(2017,01,06,7,00).strftime('%s'))
-zrEnd = int(datetime.datetime(2017,01,07,7,00).strftime('%s'))
+print(datetime.datetime(2017, 1, 7))
+zrStart = int(datetime.datetime(2017, 1, 6, 7, 0).strftime('%s'))
+zrEnd = int(datetime.datetime(2017, 1, 7, 7, 0).strftime('%s'))
+
 
 def getConsumers():
     return r.keys(pattern='cef_consumer:*')
@@ -27,15 +29,15 @@ def getStats(cef, zrstart, zrend):
     zldata = []
     zrData = []
     for reading in r.zrangebyscore(cef,zrStart,zrEnd):
-        zldata.append(json.loads(reading))
+        zldata.append(json.loads(bytes(reading).decode('utf-8')))
 
     for values in zldata:
         #  datetime.datetime.fromtimestamp(values['date']).strftime('%c')
-        # yield values['cef_consumerId'], values['count'], values['date']
-        zrData.append([values['cef_consumerId'], values['count'], values['date']])
-    return zrData
+        yield values['cef_consumerId'], values['count'], values['date']
+        #zrData.append([values['cef_consumerId'], values['count'], values['date']])
+    #return zrData
 
-def allConsumersStats(zrstart,zrend):
+def allConsumersStats(zrstart, zrend):
     allConsumers = dict()
     for consumer in getConsumers():
         allConsumers[consumer] = []
@@ -46,7 +48,7 @@ def allConsumersStats(zrstart,zrend):
 for item in allConsumersStats(zrStart, zrEnd).items():
     # print item[0]
     for dp in item[1]:
-        print [datetime.datetime.fromtimestamp(dp[2]).strftime('%Y, %m, %d, %H, %M'), dp[0], dp[1]]
+        print([datetime.datetime.fromtimestamp(dp[2]).strftime('%Y, %m, %d, %H, %M'), dp[0], dp[1]])
 
 #  store the call to getStats into var first and iterate over it for x,y values rather than call it twice.
 #zrData = getStats("cef_consumer:docker1-1", zrStart, zrEnd)
@@ -54,43 +56,38 @@ for item in allConsumersStats(zrStart, zrEnd).items():
 #yvals = [int(y[1]) for y in zrData]
 
 
-@app.route('/pygal')
-def pygal():
-    # create a bar chart
-    title = 'Temps for %s, %s on %s' % (city, state, date)
-    bar_chart = pygal.Bar(width=1200, height=600, explicit_size=True, title=title, style=DarkSolarizedStyle, disable_xml_declaration=True)
-    bar_chart.x_labels = times
-    bar_chart.add('Temps in F', imp_temps)
 
-    return render_template('cef_pygal.html', title=title, bar_chart=bar_chart)
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
 # TODO def func to prepare data for the graph.
-def getExchangeRates():
-    rates = []
-    response = urllib2.urlopen('http://api.fixer.io/latest')
-    data = response.read()
-    rdata = json.loads(data, parse_float=float)
 
-    rates.append( rdata['rates']['USD'] )
-    rates.append( rdata['rates']['GBP'] )
-    rates.append( rdata['rates']['HKD'] )
-    rates.append( rdata['rates']['AUD'] )
-    return rates
 
 @app.route("/")
-def index():
-    rates = getExchangeRates()
-    return render_template('gg.html',**locals())
+def cef_consumer_stats():
+    # create a bar chart
+    title = "CEF Consumer" #'Temps for %s, %s on %s'
+
+    date_chart = pygal.Line(x_label_rotation=20)
+    date_chart.x_labels = map(lambda d: d.strftime('%Y-%m-%d'), [
+        datetime.datetime(2013, 1, 2),
+        datetime.datetime(2013, 1, 12),
+        datetime.datetime(2013, 2, 2),
+        datetime.datetime(2013, 2, 22)])
+    date_chart.add("Visits", [300, 412, 823, 672])
+    date_chart.add("Dates", [320, 212, 623, 972])
+
+    #date_chart.render()
+    return render_template('cef_pygal.html',
+                           title="Date", style=DarkSolarizedStyle,
+                           date_chart=date_chart)
+
+
 
 @app.route("/hello")
 def hello():
-    return "Hello World!"
-
-
-
+    return "<h1>Hello</h1>"
 
 
 if __name__ == "__main__":
