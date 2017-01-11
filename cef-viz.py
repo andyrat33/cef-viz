aa11455c8b47d36753c063705d15706bf40b1313
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2017-01-11 15:08:34 +0000

    used a single x axis for the graph so that all dp appear to align although in reality the samples occur at different time indexes

--- cef-viz.py
+++ cef-viz.py
@@ -17,8 +17,8 @@ rconfig = {
 r = redis.StrictRedis(**rconfig)
 # Time Range for Selection - hardcoded for now
 print(datetime.datetime(2017, 1, 7))
-zrStart = int(datetime.datetime(2017, 1, 6, 7, 0).strftime('%s'))
-zrEnd = int(datetime.datetime(2017, 1, 7, 7, 0).strftime('%s'))
+zrStart = int(datetime.datetime(2017, 1, 7, 7, 0).strftime('%s'))
+zrEnd = int(datetime.datetime(2017, 1, 7, 9, 0).strftime('%s'))
 
 
 def getConsumers():
@@ -27,15 +27,13 @@ def getConsumers():
 
 def getStats(cef, zrstart, zrend):
     zldata = []
-    zrData = []
     for reading in r.zrangebyscore(cef,zrStart,zrEnd):
         zldata.append(json.loads(bytes(reading).decode('utf-8')))
 
     for values in zldata:
         #  datetime.datetime.fromtimestamp(values['date']).strftime('%c')
         yield values['cef_consumerId'], values['count'], values['date']
-        #zrData.append([values['cef_consumerId'], values['count'], values['date']])
-    #return zrData
+
 
 def allConsumersStats(zrstart, zrend):
     allConsumers = dict()
@@ -44,19 +42,6 @@ def allConsumersStats(zrstart, zrend):
         for record in getStats(consumer, zrStart, zrEnd):
             allConsumers[consumer].append(record)
     return allConsumers
-
-for item in allConsumersStats(zrStart, zrEnd).items():
-    # print item[0]
-    for dp in item[1]:
-        print([datetime.datetime.fromtimestamp(dp[2]).strftime('%Y, %m, %d, %H, %M'), dp[0], dp[1]])
-
-#  store the call to getStats into var first and iterate over it for x,y values rather than call it twice.
-#zrData = getStats("cef_consumer:docker1-1", zrStart, zrEnd)
-#xdate = map(lambda d: d.strftime('%c'), [datetime.datetime.fromtimestamp(x[2]) for x in zrData])
-#yvals = [int(y[1]) for y in zrData]
-
-
-
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -67,20 +52,30 @@ app = Flask(__name__, template_folder=tmpl_dir)
 @app.route("/")
 def cef_consumer_stats():
     # create a bar chart
+    done = False
+    date_chart = pygal.Line(x_label_rotation=20)
+    date_chart.x_labels = []
+    for item in allConsumersStats(zrStart, zrEnd).items():
+        # print item[0]
+        nums = []
+        for dp in item[1]:
+            # TODO use a set for dates in x_labels
+            if not done:
+                date_chart.x_labels.append(datetime.datetime.fromtimestamp(dp[2]).strftime('%Y-%m-%d-%H-%M'))
+
+            nums.append(dp[1])
+            cefname = dp[0]
+        done = True
+        date_chart.add(cefname, nums)
+
+    #date_chart.x_labels = list(set(str(date_chart.x_labels[x]) for x in range(len(date_chart.x_labels))))[0:len(nums)]
+    # Could use start and end time as the scale???
+
     title = "CEF Consumer" #'Temps for %s, %s on %s'
 
-    date_chart = pygal.Line(x_label_rotation=20)
-    date_chart.x_labels = map(lambda d: d.strftime('%Y-%m-%d'), [
-        datetime.datetime(2013, 1, 2),
-        datetime.datetime(2013, 1, 12),
-        datetime.datetime(2013, 2, 2),
-        datetime.datetime(2013, 2, 22)])
-    date_chart.add("Visits", [300, 412, 823, 672])
-    date_chart.add("Dates", [320, 212, 623, 972])
 
-    #date_chart.render()
     return render_template('cef_pygal.html',
-                           title="Date", style=DarkSolarizedStyle,
+                           title=title, style=DarkSolarizedStyle,
                            date_chart=date_chart)
 
 
