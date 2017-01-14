9c0e013072124d3cd58b66951df12f14e7de1039
andyrat33 <andrew.ratcliffe@nswcsystems.co.uk>
2017-01-14 16:31:13 +0000

    updated templates and added links from '/'. Removed some of the parameters as using a few globals was easier.

--- cef-viz.py
+++ cef-viz.py
@@ -1,43 +1,54 @@
 import os
 import json
 import redis
+import pygal
 from datetime import datetime, timedelta
-
 from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 from flask_wtf import Form
 from wtforms import DateTimeField
-
-
-
-import pygal
 from pygal.style import DarkSolarizedStyle
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-print(app.secret_key)
 rconfig = {
     'host': 'docker2',
     'port': 6379,
     'db': 0,
 }
 
+sEnd = datetime.now()
+sStart = datetime.now()
+zrEnd = 0
+zrStart = 0
+
 r = redis.StrictRedis(**rconfig)
 
-sEnd = datetime.now()
-sStart = (sEnd - timedelta(minutes=60))
+def updateDateTime():
+    global sEnd, sStart, zrEnd, zrStart
+    sEnd = datetime.now()
+    sStart = (sEnd - timedelta(minutes=60))
+    zrEnd = int(sEnd.strftime('%s'))
+    zrStart = int(sStart.strftime('%s'))
 
+updateDateTime()
 
-zrEnd = int(sEnd.strftime('%s'))
-zrStart = int(sStart.strftime('%s'))
 
 # Done. Create defaults for start and end = now() and start = now() - 1h
 
 
-def chkTimeDelta(zrEnd, zrStart, diff=10):
-    cdiff = (zrEnd - zrStart)
+def chkTimeDelta(dtEnd: datetime, dtStart: datetime, diff: int = 10) -> bool:
+    """
+    This function returns true or false. Start time must be less than end time by amount diff and start time must be at
+    least 10 minutes in the past
+    :param dtEnd:
+    :param dtStart:
+    :param diff:
+    :return: bool
+    """
+    cdiff = (dtEnd - dtStart)
     # Check that 10 mins or more occurs in the past
-    ndiff = (datetime.now() - zrStart)
+    ndiff = (datetime.now() - dtStart)
     if cdiff >= timedelta(minutes=diff) and ndiff >= timedelta(minutes=10):
         return True
     else:
@@ -54,7 +65,7 @@ def getConsumers():
     return r.keys(pattern='cef_consumer:*')
 
 
-def getStats(cef, zrstart, zrend):
+def getStats(cef):
     zldata = []
     for reading in r.zrangebyscore(cef,zrStart,zrEnd):
         zldata.append(json.loads(bytes(reading).decode('utf-8')))
@@ -63,17 +74,16 @@ def getStats(cef, zrstart, zrend):
         yield values['cef_consumerId'], values['count'], values['date']
 
 
-def allConsumersStats(zrstart, zrend):
+def allConsumersStats():
     allConsumers = dict()
     for consumer in getConsumers():
         allConsumers[consumer] = []
-        for record in getStats(consumer, zrStart, zrEnd):
+        for record in getStats(consumer):
             allConsumers[consumer].append(record)
     return allConsumers
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-
 
 
 @app.route("/")
@@ -82,7 +92,7 @@ def cef_consumer_stats():
     done = False
     date_chart = pygal.Line(x_label_rotation=20)
     date_chart.x_labels = []
-    for item in allConsumersStats(zrStart, zrEnd).items():
+    for item in allConsumersStats().items():
         # print item[0]
         nums = []
         for dp in item[1]:
@@ -96,12 +106,12 @@ def cef_consumer_stats():
         done = True
         date_chart.add(cefname, nums)
 
-
-    title = "CEF Consumer"
-
+    title = "CEF Consumer Graph"
 
     return render_template('cef_pygal.html',
-                           title=title, style=DarkSolarizedStyle,
+                           title=title, StartDate=sStart.strftime('%Y-%m-%d %H:%M'),
+                           EndDate=sEnd.strftime('%Y-%m-%d %H:%M'),
+                           style=DarkSolarizedStyle,
                            date_chart=date_chart)
 
 
@@ -109,7 +119,7 @@ def cef_consumer_stats():
 @app.route("/eps")
 def eps():
     result = dict()
-    for item in allConsumersStats(zrStart, zrEnd).items():
+    for item in allConsumersStats().items():
         # print item[0]
         total_events = 0
         dcount = 0
@@ -132,14 +142,15 @@ def total():
 
 @app.route('/submit', methods=('GET', 'POST'))
 def submit():
-    global zrStart, zrEnd
+    global zrStart, zrEnd, sStart, sEnd
+
     form = dataEntry(request.form, csrf_enabled=False)
     if request.method == 'POST' and form.validate():
-        start = form.uiStartDateTime.data
-        end = form.uiEndDateTime.data
-        if chkTimeDelta(end, start):
-            zrStart = int(start.strftime('%s'))
-            zrEnd = int(end.strftime('%s'))
+        sStart = form.uiStartDateTime.data
+        sEnd = form.uiEndDateTime.data
+        if chkTimeDelta(sEnd, sStart):
+            zrStart = int(sStart.strftime('%s'))
+            zrEnd = int(sEnd.strftime('%s'))
             return redirect(url_for('cef_consumer_stats'))
     return render_template('submit.html', title='Date and Time Range', form=form)
 
