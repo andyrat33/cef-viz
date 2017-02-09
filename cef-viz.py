ed2d5773f6be55ef5c5b5a77aca0eca7a9bbea23
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2017-02-09 00:15:02 +0000

    adapted allConsumerStats and added get_consumer_stats and eps

--- cef-viz.py
+++ cef-viz.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top secret!'
 bootstrap = Bootstrap(app)
 
-# store the config in cef-viz.cfg
+# store the config in cefviz.ini
 def get_config():
     conf = ConfigParser()
     conf.read('config/cefviz.ini')
@@ -43,13 +43,19 @@ def updateDateTime():
 
 updateDateTime()
 
+
 def get_all_consumers():
+
     return r.keys(pattern='cef_consumer:*')
 
+
 def build_choices():
+
     return [((bytes(x).decode('utf-8')),(bytes(x).decode('utf-8'))) for x in get_all_consumers()]
 
+
 cef_name = build_choices()[0][1]
+
 
 class dataEntry(Form):
     uiStartDateTime = DateTimeField('Start date time', format='%Y-%m-%d %H:%M', default=sStart, validators=[Required()])
@@ -83,8 +89,9 @@ class dataEntry(Form):
         return result
 
 
-def getConsumers(consumer_name):
-    return r.keys(pattern=consumer_name)
+def get_consumer(consumer_pattern):
+    return r.keys(pattern=consumer_pattern)
+
 
 def getStats(cef):
     zldata = []
@@ -96,23 +103,50 @@ def getStats(cef):
 
 
 def allConsumersStats():
-    global cef_name
+    #global cef_name
     allConsumers = dict()
-    for consumer in getConsumers(cef_name):
+    for consumer in get_all_consumers():
         allConsumers[consumer] = []
+        # Use a call to consumer_stats(consumer)
         for record in getStats(consumer):
             allConsumers[consumer].append(record)
     return allConsumers
 
 
+def consumer_stats(cef=cef_name):
+    constatdata = dict()
+    constatdata[cef] = []
+    for record in getStats(cef):
+        constatdata[cef].append(record)
+
+    return constatdata
+
+def eps():
+
+    result = dict()
+    for item in allConsumersStats().items():
+        # print item[0]
+        total_events = 0
+        dcount = 0
+        for dp in item[1]:
+            # EPS = num events / divided by 600 seconds collection period
+            total_events += dp[1]
+            dcount += 1
+        try:
+            result[dp[0]] = total_events / (dcount * 600)
+        except:
+            result[dp[0]] = 0
+
+    return result
 
 def cef_consumer_chart():
     """Create a graph for the cef_consumer"""
-    global cef_name
+    #global cef_name
     done = False
     date_chart = pygal.Line(x_label_rotation=20)
     date_chart.x_labels = []
-    for item in allConsumersStats().items():
+    #for item in allConsumersStats().items():
+    for item in consumer_stats(cef_name).items():
         # print item[0]
         nums = []
         for dp in item[1]:
@@ -144,7 +178,7 @@ def index():
         cef_name = form.cef_consumer_instance.data
 
     return render_template('index.html', form=form, total_count=total_count,
-                           date_chart=cef_consumer_chart())
+                           date_chart=cef_consumer_chart(), eps_results=eps())
 
 if __name__ == '__main__':
     app.run(debug=True)
